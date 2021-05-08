dcf9b4ec6d24e3563676b8c38eb2f50112fa396b
Maria Shvyrkova <shvyrkovamaria@gmail.com>
2021-05-08 11:36:26 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,40 @@
-import './styles.css';
+const colors = [
+  '#FFFFFF',
+  '#2196F3',
+  '#4CAF50',
+  '#FF9800',
+  '#009688',
+  '#795548',
+];
+
+const DELAY = 1000;
+let startIntervalId = null;
+let isActive = false;
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+const refs = {
+  btnStart: document.querySelector('[data-action="start"]'),
+  btnStop: document.querySelector('[data-action="stop"]'),
+};
+
+refs.btnStart.addEventListener('click', onBtnStartClick);
+refs.btnStop.addEventListener('click', onBtnStopClick);
+
+function onBtnStartClick() {
+  if (isActive) {
+    return;
+  }
+  isActive = true;
+  startIntervalId = setInterval(changeBgColor, DELAY);
+}
+
+function onBtnStopClick() {
+  clearInterval(startIntervalId);
+}
+
+function changeBgColor() {
+  let currentColorIndex = randomIntegerFromInterval(0, colors.length);
+  document.body.style.backgroundColor = colors[currentColorIndex];
+}
